2a94e292546a589195a404fc92b41ef58f7f2883
uDay-selise <saiful.uday@gmail.com>
2024-10-29 15:18:22 +0600

    fix deisn of sigin page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,10 +5,10 @@ import bgAuth from "@/assets/bg-auth.png";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen">
-      <div className="flex-1 flex justify-center items-center">
-        <div className="w-[400px]">{children}</div>
+      <div className="flex-1 flex justify-center items-center px-4">
+        <div className="w-full max-w-[400px]">{children}</div>
       </div>
-      <div className="flex-1 relative">
+      <div className="hidden md:block flex-1 relative">
         <Image src={bgAuth} alt="bg-auth" fill={true} />
         <div className=" absolute left-0 right-0 top-0 bottom-0 bg-primary opacity-70"></div>
       </div>
