2a2e2df3960129d7508a9bf32f657f7ae106ab05
uDay-selise <saiful.uday@selisegroup.com>
2024-11-30 19:57:56 +0600

    change env

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -5,8 +5,6 @@ export const signin = async (data: { username: string; password: string }) => {
   formData.append("grant_type", "password");
   formData.append("username", data.username);
   formData.append("password", data.password);
-  // const backendUrl =
-  //   "https://dev-msblocks.seliselocal.com/api/authentication/v1/oauth/token";
   const ownApiUrl = "/api/auth/signin";
   return clients.post(ownApiUrl, formData, {
     "Content-Type": "application/x-www-form-urlencoded",
