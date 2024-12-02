a7bc38ddbc6abf3f821f1fb50293db85345e9ccb
uDay-selise <saiful.uday@selisegroup.com>
2024-12-02 16:29:47 +0600

    add hooks and service for account activation

--- src/features/auth/hooks/useAuth.tsx
+++ src/features/auth/hooks/useAuth.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { signin } from "../services/auth.service";
+import { accountActivation, signin } from "../services/auth.service";
 import { useToast } from "@/hooks/use-toast";
 
 export const useSigninMutation = () => {
@@ -13,6 +13,30 @@ export const useSigninMutation = () => {
         color: "blue",
         title: "Sucesss",
         description: "You are sucessfully logged in",
+      });
+    },
+    onError: ({ error }: { error: { errorBody: { error: string } } }) => {
+      toast({
+        variant: "destructive",
+        color: "blue",
+        title: "Error",
+        description: error.errorBody.error,
+      });
+    },
+  });
+};
+
+export const useAccountActivation = () => {
+  const { toast } = useToast();
+
+  return useMutation({
+    mutationKey: ["accountActivation"],
+    mutationFn: accountActivation,
+    onSuccess: () => {
+      toast({
+        color: "blue",
+        title: "Sucesss",
+        description: "You are sucessfully acctivated your account",
       });
     },
     onError: ({ error }: { error: { errorBody: { error: string } } }) => {
