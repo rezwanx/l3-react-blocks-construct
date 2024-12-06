83af7fa65cc6e5e83b2723c2c769835b2486eff0
uDay-selise <saiful.uday@selisegroup.com>
2024-12-06 23:35:25 +0600

    add authentication

--- src/state/client-middleware.tsx
+++ src/state/client-middleware.tsx
@@ -1,21 +1,48 @@
 "use client";
-// import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
 
-export const publicRoutes = [
-  "/signin",
-  "/signup",
-  "/forgetpassword",
-  "/resetpassword",
-  "/activate",
-  "/activate-success",
-];
+import { usePathname, useRouter } from "next/navigation";
+import { ReactNode, useEffect, useState } from "react";
 
-export const ClientMiddlware = ({ children }: { children: ReactNode }) => {
-  // const router = useRouter();
+export const publicRoutes = ["/signin", "/activate", "/activate-success"];
 
-  // if (!localStorage.getItem("access_token")) {
-  //   router.replace("/signin");
-  // }
+const useAuthState = () => {
+  const [isMounted, setMounted] = useState(false);
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    setHasToken(!!localStorage.getItem("access_token"));
+  }, []);
+
+  return { isMounted, hasToken };
+};
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+  const currentPath = usePathname();
+  const { isMounted, hasToken } = useAuthState();
+
+  useEffect(() => {
+    if (isMounted && !hasToken && currentPath !== "/signin") {
+      router.push("/signin");
+    }
+  }, [isMounted, hasToken, currentPath, router]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  if (!hasToken && currentPath !== "/signin") {
+    return null;
+  }
   return <>{children}</>;
+};
+
+export const ClientMiddleware = ({ children }: { children: ReactNode }) => {
+  const currentPath = usePathname();
+  const isPublicRoute = publicRoutes.includes(currentPath);
+
+  if (isPublicRoute) return <>{children}</>;
+
+  return <AuthProvider>{children}</AuthProvider>;
 };
