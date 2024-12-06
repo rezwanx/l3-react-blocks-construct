88161324f49d21be451351386cb2ff584438a8b3
uDay-selise <saiful.uday@selisegroup.com>
2024-12-07 00:02:59 +0600

    fix type issue

--- src/components/core/u-password-input/u-password-input.tsx
+++ src/components/core/u-password-input/u-password-input.tsx
@@ -1,38 +1,23 @@
 "use client";
-
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { UPasswordInputProps } from "./index.type";
+import { Input, InputProps } from "@/components/ui/input";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 
-export function UPasswordInput({
-  label,
-  placeholder = "",
-  error,
-  ...rest
-}: UPasswordInputProps) {
-  const [open, setOpen] = useState(false);
-  const EyeComponet = open ? EyeOffIcon : EyeIcon;
-  return (
-    <div className="grid w-full items-center gap-2">
-      {label && (
-        <Label htmlFor={label} className="text-primary">
-          {label}
-        </Label>
-      )}
-      <div className="relative">
-        <Input
-          type={open ? "text" : "password"}
-          id={label}
-          placeholder={placeholder}
-          {...rest}
-        />
-        <EyeComponet
-          className="size-5 text-primary absolute top-2 right-3"
-          onClick={() => setOpen(!open)}
-        />
+export const UPasswordInput = forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    const [open, setOpen] = useState(false);
+    const EyeComponet = open ? EyeOffIcon : EyeIcon;
+    return (
+      <div className="grid w-full items-center gap-2">
+        <div className="relative">
+          <Input type={open ? "text" : "password"} {...props} ref={ref} />
+          <EyeComponet
+            className="size-5 text-primary absolute top-2 right-3"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+UPasswordInput.displayName = "UPasswordInput";
--- src/features/auth/components/signin-form/signing-form.tsx
+++ src/features/auth/components/signin-form/signing-form.tsx
@@ -64,7 +64,7 @@ export const SigninForm = () => {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <UPasswordInput {...field} error="" />
+                <UPasswordInput {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
