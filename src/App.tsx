782a0364540fb295c420e511b06cde8303d8655b
afiapreety <afia.preety@selisegroup.com>
2025-01-09 10:00:03 +0600

    fix: tailwind with typescript is working now

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-3xl font-bold underline text-red-500">
+    Hello world!
+  </div>
+  );
+}
+
+export default App;
