4f67aef07f1dd2c3b86a62718f4ff9231aee62ee
afiapreety <afia.preety@selisegroup.com>
2025-01-22 02:50:07 +0600

    fix: removed regex security related issues

--- src/features/auth/components/set-password/utils.ts
+++ src/features/auth/components/set-password/utils.ts
@@ -1,15 +1,26 @@
 import { z } from 'zod';
 
+const hasLowercase = /[a-z]/;
+const hasUppercase = /[A-Z]/;
+const hasNumber = /[0-9]/;
+const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
+
 export const setPasswordFormValidationSchema = z
   .object({
     password: z
       .string()
       .min(8, 'Password must be at least 8 characters long')
-      .regex(/(?=.*[a-z])/, 'Password must contain at least one lowercase letter')
-      .regex(/(?=.*[A-Z])/, 'Password must contain at least one uppercase letter')
-      .regex(/(?=.*\d)/, 'Password must contain at least one number')
-      .regex(
-        /(?=.*[!@#$%^&*(),.?":{}|<>])/,
+      .refine(
+        (password) => hasLowercase.test(password),
+        'Password must contain at least one lowercase letter'
+      )
+      .refine(
+        (password) => hasUppercase.test(password),
+        'Password must contain at least one uppercase letter'
+      )
+      .refine((password) => hasNumber.test(password), 'Password must contain at least one number')
+      .refine(
+        (password) => hasSpecialChar.test(password),
         'Password must contain at least one special character'
       ),
     confirmPassword: z.string().min(8, 'Password must be at least 8 characters long'),
