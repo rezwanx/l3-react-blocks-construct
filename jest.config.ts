3bb67371a8b90e39be2f1159c466f5bb29d1cc15
afiapreety <afia.preety@selisegroup.com>
2025-01-22 13:42:32 +0600

    fix: checking jest setup issue on sonarqube

--- jest.config.ts
+++ jest.config.ts
@@ -1,42 +1,36 @@
-// jest.config.ts
 import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^features/(.*)$': '<rootDir>/src/features/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-
-  // Test file pattern
-  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text', 'clover'],
+  testMatch: ['**/*.spec.{ts,tsx}'],
   collectCoverage: true,
   coverageProvider: 'v8',
-
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
-    '!src/**/*.test.{ts,tsx}',
     '!src/**/*.spec.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/*.model.ts',
+    '!src/**/*.module.ts',
     '!src/**/*.d.ts',
-    '!src/main.tsx',
-    '!src/vite-env.d.ts',
     '!**/node_modules/**',
   ],
-
-  // Make test fail if coverage is too low
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.jest.json',
+      },
+    ],
   },
 };
 
