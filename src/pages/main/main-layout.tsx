291a8ced837560a152629c1cd6ef215e55a67a26
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-02-03 20:27:17 +0600

    fix(blocks-construct): fix the height issue in the main layout when we search items

--- src/pages/main/main-layout.tsx
+++ src/pages/main/main-layout.tsx
@@ -26,7 +26,7 @@ export default function MainLayout() {
               <UProfileMenu />
             </div>
           </div>
-          <div className="flex bg-surface p-4 sm:p-8">
+          <div className="flex h-full bg-surface p-4 sm:p-8">
             <Outlet />
           </div>
         </div>
