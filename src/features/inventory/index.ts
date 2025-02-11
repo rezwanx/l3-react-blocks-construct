d11dd899a7fed3bbb3d3d1714f64e8dfc55479e3
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-02-11 16:29:50 +0600

    refactor(blocks-construct): code refactor in the inventory table component

---
+++ src/features/inventory/index.ts
@@ -0,0 +1,1 @@
+export { InventoryAdvanceTable } from './component/inventory-advance-table/inventory-advance-table';
--- src/pages/inventory/inventory.tsx
+++ src/pages/inventory/inventory.tsx
@@ -1,9 +1,12 @@
+import InventoryAdvanceTable from 'features/inventory/component/inventory-advance-table/inventory-advance-table';
+
 export function Inventory() {
   return (
     <div className="flex w-full flex-col">
       <div className="mb-[18px] flex items-center text-base text-high-emphasis md:mb-[24px]">
-        <h3 className="text-2xl font-bold tracking-tight">Inventory Advance table</h3>
+        <h3 className="text-2xl font-bold tracking-tight">Inventory</h3>
       </div>
+      <InventoryAdvanceTable />
     </div>
   );
 }
