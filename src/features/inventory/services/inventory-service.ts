8521c49861d889825227f08c2d968c08906dd132
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-02-11 17:19:04 +0600

    refactor(blocks-construct): code refactor

---
+++ src/features/inventory/services/inventory-service.ts
@@ -0,0 +1,11 @@
+export interface InventoryData {
+  itemId: string;
+  itemsName: string;
+  category: string;
+  supplier: string;
+  itemLoc: string;
+  stock: number | null;
+  lastupdated: string;
+  price: string;
+  status: boolean;
+}
