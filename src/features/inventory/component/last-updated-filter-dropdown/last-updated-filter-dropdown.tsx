0dfc57be4fe437e0d9e822a6deea95d617255fdb
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-02-27 18:07:16 +0600

    refactor(blocks-construct): disabled code line lint issue

--- src/features/inventory/component/last-updated-filter-dropdown/last-updated-filter-dropdown.tsx
+++ src/features/inventory/component/last-updated-filter-dropdown/last-updated-filter-dropdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { DateRange } from 'react-day-picker';
 import { format } from 'date-fns';
--- src/features/inventory/component/stock-filter-dropdown/stock-filter-dropdown.tsx
+++ src/features/inventory/component/stock-filter-dropdown/stock-filter-dropdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { Input } from 'components/ui/input';
 import { Updater } from '@tanstack/react-table';
