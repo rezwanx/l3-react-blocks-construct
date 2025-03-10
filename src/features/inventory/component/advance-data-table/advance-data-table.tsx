902ebc64d26e189138a714227633c04b75459994
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-03-10 11:58:26 +0600

    refactor(blocks-construct): resolved the sonarQube issue in the advance table

--- src/features/inventory/component/advance-data-table/advance-data-table.tsx
+++ src/features/inventory/component/advance-data-table/advance-data-table.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import clsx from 'clsx';
 import { v4 as uuidv4 } from 'uuid';
@@ -20,6 +20,8 @@ import {
   ColumnPinningState,
   getGroupedRowModel,
   Column,
+  Row,
+  Cell,
 } from '@tanstack/react-table';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from 'components/ui/table';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from 'components/ui/card';
@@ -155,76 +157,72 @@ export function AdvanceDataTable<TData, TValue>({
   );
 
   const renderTableBodyContent = () => {
-    if (isLoading) {
-      return renderSkeletonRows();
-    }
-    if (error) {
-      return renderErrorRow(error);
-    }
-    if (table.getRowModel().rows.length) {
-      return table.getRowModel().rows.map((row) => (
-        <>
-          <TableRow
-            key={row.id}
-            data-state={row.getIsSelected() && 'selected'}
-            onClick={() => onRowClick?.(row.original)}
-            className={row.getIsSelected() ? '!bg-primary-50' : 'cursor-pointer'}
-          >
-            {row.getVisibleCells().map((cell) => {
-              const { column } = cell;
-              return (
-                <TableCell
-                  key={cell.id}
-                  className={`pl-4 py-4 ${row.getIsSelected() && '!bg-primary-50'} ${getCommonPinningClasses(column)}`}
-                  style={{
-                    left:
-                      column.getIsPinned() === 'left' ? `${column.getStart('left')}px` : undefined,
-                    right:
-                      column.getIsPinned() === 'right'
-                        ? `${column.getAfter('right')}px`
-                        : undefined,
-                    width: column.getSize(),
-                  }}
-                >
-                  {cell.column.id === 'select' ? (
-                    <div className="flex items-center gap-2">
-                      <Checkbox
-                        checked={row.getIsSelected()}
-                        onCheckedChange={(value) => row.toggleSelected(!!value)}
-                        onClick={(e) => e.stopPropagation()}
-                        aria-label="Select row"
-                        className="border-medium-emphasis data-[state=checked]:border-none border-2"
-                      />
-                      {isExpandRowContent && (
-                        <Button
-                          size="icon"
-                          variant="ghost"
-                          className="rounded-full"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            row.toggleExpanded();
-                          }}
-                        >
-                          {row.getIsExpanded() ? <ChevronUp /> : <ChevronDown />}
-                        </Button>
-                      )}
-                    </div>
-                  ) : (
-                    flexRender(cell.column.columnDef.cell, cell.getContext())
-                  )}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-          {isExpandRowContent &&
-            row.getIsExpanded() &&
-            expandRowContent &&
-            expandRowContent(row.id, columns.length)}
-        </>
-      ));
-    }
-    return renderNoResultFound();
+    if (isLoading) return renderSkeletonRows();
+    if (error) return renderErrorRow(error);
+    if (!table.getRowModel().rows.length) return renderNoResultFound();
+
+    return table.getRowModel().rows.map((row) => (
+      <React.Fragment key={row.id}>
+        {renderTableRow(row)}
+        {isExpandRowContent && row.getIsExpanded() && expandRowContent?.(row.id, columns.length)}
+      </React.Fragment>
+    ));
   };
+
+  const renderTableRow = (row: Row<TData>) => (
+    <TableRow
+      key={row.id}
+      data-state={row.getIsSelected() && 'selected'}
+      onClick={() => onRowClick?.(row.original)}
+      className={row.getIsSelected() ? '!bg-primary-50' : 'cursor-pointer'}
+    >
+      {row.getVisibleCells().map((cell) => renderTableCell(cell, row))}
+    </TableRow>
+  );
+
+  const renderTableCell = (cell: Cell<TData, unknown>, row: Row<TData>) => {
+    const { column } = cell;
+    return (
+      <TableCell
+        key={cell.id}
+        className={`pl-4 py-4 ${row.getIsSelected() && '!bg-primary-50'} ${getCommonPinningClasses(column)}`}
+        style={{
+          left: column.getIsPinned() === 'left' ? `${column.getStart('left')}px` : undefined,
+          right: column.getIsPinned() === 'right' ? `${column.getAfter('right')}px` : undefined,
+          width: column.getSize(),
+        }}
+      >
+        {cell.column.id === 'select'
+          ? renderSelectCell(row)
+          : flexRender(cell.column.columnDef.cell, cell.getContext())}
+      </TableCell>
+    );
+  };
+
+  const renderSelectCell = (row: Row<TData>) => (
+    <div className="flex items-center gap-2">
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        onClick={(e) => e.stopPropagation()}
+        aria-label="Select row"
+        className="border-medium-emphasis data-[state=checked]:border-none border-2"
+      />
+      {isExpandRowContent && (
+        <Button
+          size="icon"
+          variant="ghost"
+          className="rounded-full"
+          onClick={(e) => {
+            e.stopPropagation();
+            row.toggleExpanded();
+          }}
+        >
+          {row.getIsExpanded() ? <ChevronUp /> : <ChevronDown />}
+        </Button>
+      )}
+    </div>
+  );
 
   const getCommonPinningClasses = (column: Column<TData, unknown>) => {
     const isPinned = column.getIsPinned();
