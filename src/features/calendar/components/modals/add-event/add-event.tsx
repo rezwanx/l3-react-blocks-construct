8724c45e301addf006b6292d5c6ea5e59678a47c
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-04-02 14:13:10 +0600

    feat(blocks-construct): menubar for the members

--- src/features/calendar/components/modals/add-event/add-event.tsx
+++ src/features/calendar/components/modals/add-event/add-event.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { format } from 'date-fns';
-import { CalendarIcon, Plus, Trash } from 'lucide-react';
+import { CalendarIcon, Plus, Search, Trash } from 'lucide-react';
 import { Button } from 'components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel } from 'components/ui/form';
 import { Input } from 'components/ui/input';
@@ -26,6 +26,8 @@ import { Separator } from 'components/ui/separator';
 import { Switch } from 'components/ui/switch';
 import { Calendar } from 'components/ui/calendar';
 import { Label } from 'components/ui/label';
+import { Checkbox } from 'components/ui/checkbox';
+import { Menubar, MenubarContent, MenubarMenu, MenubarTrigger } from 'components/ui/menubar';
 import { AddEventFormValues, formSchema } from '../../../utils/form-schema';
 import { ColorPickerTool } from '../../color-picker-tool/color-picker-tool';
 
@@ -83,9 +85,59 @@ export function AddEvent({ start, end, onSubmit, onCancel }: Readonly<AddEventPr
                 <AvatarImage alt="participants" src="https://github.com/shadcn.png" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
-              <Button variant="outline" size="icon" className="border-dashed">
-                <Plus className="w-5 h-5 text-high-emphasis" />
-              </Button>
+              <Menubar className="border-none">
+                <MenubarMenu>
+                  <MenubarTrigger asChild>
+                    <Button variant="outline" size="icon" className="border-dashed">
+                      <Plus className="w-5 h-5 text-high-emphasis" />
+                    </Button>
+                  </MenubarTrigger>
+                  <MenubarContent className="flex flex-col gap-6 py-2 px-3">
+                    <div className="flex flex-col gap-2">
+                      <p className="font-semibold text-base text-high-emphasis">Members</p>
+                      <div className="relative">
+                        <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+                        <Input
+                          placeholder="Search members"
+                          className="h-10 w-full rounded-lg bg-surface border-none pl-8"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <div className="flex items-center gap-2">
+                        <Checkbox className="border-medium-emphasis data-[state=checked]:border-none border-2 rounded-[2px]" />
+                        <Avatar className="ring-2 ring-neutral-50 w-6 h-6 shadow-md">
+                          <AvatarImage alt="participants" src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <label htmlFor="member1" className="text-sm font-normal text-high-emphasis">
+                          Aaron Green
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox className="border-medium-emphasis data-[state=checked]:border-none border-2 rounded-[2px]" />
+                        <Avatar className="ring-2 ring-neutral-50 w-6 h-6 shadow-md">
+                          <AvatarImage alt="participants" src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <label htmlFor="member1" className="text-sm font-normal text-high-emphasis">
+                          Aaron Green
+                        </label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <Checkbox className="border-medium-emphasis data-[state=checked]:border-none border-2 rounded-[2px]" />
+                        <Avatar className="ring-2 ring-neutral-50 w-6 h-6 shadow-md">
+                          <AvatarImage alt="participants" src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <label htmlFor="member1" className="text-sm font-normal text-high-emphasis">
+                          Aaron Green
+                        </label>
+                      </div>
+                    </div>
+                  </MenubarContent>
+                </MenubarMenu>
+              </Menubar>
             </div>
           </div>
           <div className="flex w-full gap-4">
