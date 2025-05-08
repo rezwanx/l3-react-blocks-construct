1eff925159b5fc79222399e6d400880f46b749ef
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-05-08 18:07:27 +0600

    fix(blocks-construct): fixed the mouseover issue in spec file

--- src/constant/sidebar-menu.spec.ts
+++ src/constant/sidebar-menu.spec.ts
@@ -3,7 +3,7 @@ import { menuItems } from '../constant/sidebar-menu';
 describe('menuItems', () => {
   test('should have the correct structure', () => {
     expect(Array.isArray(menuItems)).toBe(true);
-    expect(menuItems.length).toBe(8);
+    expect(menuItems.length).toBe(11);
   });
 
   test('each menu item should have required properties', () => {
