287ef213ba0cc4b1f877295a6af652df6b6364df
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-05-20 16:00:14 +0600

    fix(blocks-construct): resolve the sonarqube issues

--- src/features/dashboard/component/dashboard-user-activity-graph/dashboard-user-activity-graph.spec.tsx
+++ src/features/dashboard/component/dashboard-user-activity-graph/dashboard-user-activity-graph.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { DashboardUserActivityGraph } from './dashboard-user-activity-graph';
 
